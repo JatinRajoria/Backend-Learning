28291aab7dfbb0c4bf39979ac99cd9a79fa88108
JatinRajoria <115098932+JatinRajoria@users.noreply.github.com>
2025-09-09 23:51:51 +0530

    Learn express.json middleware usecase and REST API methods

---
+++ BackendDay3/server.js
@@ -0,0 +1,21 @@
+// Create notes
+const express = require('express');
+const app = express()
+
+app.use(express.json())
+
+let notes = []
+
+app.post("/notes",(req,res)=>{
+    console.log(req.body);
+    notes.push(req.body)
+    res.json({
+        message:"Note added successfully",
+        notes: notes
+    })
+})
+
+app.listen(3000,(req,res)=>{
+    console.log("Server is running on port 3000");
+})
+
